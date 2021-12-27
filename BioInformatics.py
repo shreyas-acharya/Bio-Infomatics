c379e6c4091a998b5542898f4eb5b15332dda523
Shashank-Prakash9 <64033068+Shashank-Prakash9@users.noreply.github.com>
2021-12-27 23:48:09 +0530

    Update BioInformatics.py

--- BioInformatics.py
+++ BioInformatics.py
@@ -13,7 +13,7 @@ def argument():
     args = argparse.ArgumentParser(description="BioInformatics")
     args.add_argument('--dataset_path', type=str, default='./Dataset/', help='Path to the dataset')
     args.add_argument('--epochs', type=int, default=50, help='Number of epochs')
-    args.add_argument('--target_size', nargs=3, default=(224, 224, 3), help='Size of the Input image')
+    args.add_argument('--target_size', nargs=3, default=(224, 224), help='Size of the Input image')
     args.add_argument('--batch_size', type=int, default=32, help='Batch Size')
     args.add_argument('--classes', type=int, default=9, help='Number of Classes')
     args = args.parse_args()
@@ -78,7 +78,7 @@ def main():
     MobileModel(args)
     AlexModel(args)
     VGGModel(args)
-
+    GoogleModel(args)
     
 
 main()
